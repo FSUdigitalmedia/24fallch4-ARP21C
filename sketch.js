260c4c00c555225dd9b10a80241b27b1df7b063b
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-16 07:56:07 -0400

    text has movement

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 
 let theFont;
+let y=0;
+let speed = 3;
+let deg = 0;
 
 function preload() {
  theFont = loadFont("assets/ProtestGuerrilla-Regular.ttf");
   }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   textFont(theFont);
   textSize(72);
  
@@ -15,9 +18,27 @@ function setup() {
 
 function draw() {
 
+ //bouncy text movement
+ let d = map(mouseX, 0, width, 50, 150, true);
+ background(125);
+ let yPos = map2(y, 0, height, 0, height, SINUSOIDAL, IN);
+
+//rotating the text
+push();
+translate (100,100);
+degMapped = map2(deg, 0, 90, -90, 90, SINUSOIDAL, BOTH );
+rotate( radians(degMapped));
+deg = (deg + 1) % 90;
+
+text("BOUNCY", 0, yPos);
+pop();
+if (y > height ||  y < 0) {
+ speed = speed * -1;
+}
+
+ y = y + speed;
+  
  
-  background(125);
-  text("HI", width/2, height/2);
 
   
 }

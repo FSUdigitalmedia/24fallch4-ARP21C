2066ac8bc3d34a8f328ba7054eb9ca87a165ee6c
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-16 07:20:46 -0400

    rotating rect

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let y = 0;
 let speed = 3;
+let deg = 0;
 
 
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
 }
 
 function draw() {
@@ -19,7 +21,7 @@ fill(clr);
 //draw the circle with size based on horizontal mouse position
   let d = map(mouseX, 0, width, 50, 150, true);
   background(125);
-  let yPos = map2(y, 0, height, 0, height, SINUSOIDAL, IN);
+  let yPos = map2(y, 0, height, 0, height, QUADRATIC, IN);
   circle(width/2, yPos, d);
 
 if (y > height ||  y < 0) {
@@ -28,5 +30,13 @@ if (y > height ||  y < 0) {
 
   y = y + speed;
 
+//rotating rect
+push();
+translate (100,100);
 
+degMapped = map2(deg, 0, 90, -90, 90, SINUSOIDAL, BOTH );
+rotate( radians(degMapped));
+deg = (deg + 1) % 90;
+rect(0, 0, 50, 20);
+pop();
 }

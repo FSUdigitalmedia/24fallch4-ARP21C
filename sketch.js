690af5d2d72e509076c46efd4a14334ebc004cdc
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-16 06:50:30 -0400

    change color based on vertical mouse movement

--- sketch.js
+++ sketch.js
@@ -3,6 +3,18 @@ function setup() {
 }
 
 function draw() {
+
+  //change color based on vertical mouse movement
+let startingColor = color("orange");
+let endingColor = color("blue");
+let pct = map(mouseY, 0, height, 0, 1, true);
+let clr = lerpColor(startingColor, endingColor, pct);
+fill(clr);
+
+
+
+  let d = map(mouseX, 0, width, 50, 150, true);
   background(125);
-  circle(width/2, height/2, 40);
+  circle(width/2, height/2, d);
+  console.log(d);
 }
